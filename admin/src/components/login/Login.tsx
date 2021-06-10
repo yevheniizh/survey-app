129ea920bb1eac9b674e7b973a2601c8505eed60
Pavlo Pechenevskyi <pape@ciklum.com>
2021-06-10 15:46:10 +0300

    feat: add login component

---
+++ admin/src/components/login/Login.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h2>Log in</h2>
+        </div>
+    )
+}
+
+export default Login;
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.6.5/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/8.6.5/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
+
+// npm install -g firebase-tools
+
+// You can deploy now or later. To deploy now, open a terminal window, then navigate to or create a root directory for your web app.
+
+// Sign in to Google
+// firebase login
+// Initiate your project
+// Run this command from your app’s root directory:
+
+// firebase init
+// When you’re ready, deploy your web app
+// Put your static files (e.g., HTML, CSS, JS) in your app’s deploy directory (the default is “public”). Then, run this command from your app’s root directory:
+
+// firebase deploy
+// After deploying, view your app at test-auth-140e7.web.app
+
+// Need help? Check out the Hosting docs
