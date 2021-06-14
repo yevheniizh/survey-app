031cc179d324ce49b9e6929f9e82efc8ae41ed00
Pavlo Pechenevskyi <pape@ciklum.com>
2021-06-14 09:25:02 +0300

    feat: add sign out

--- admin/src/components/authorized/Authorized.tsx
+++ admin/src/components/authorized/Authorized.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+import { MyContext } from '../..';
 
 const Authorized = () => {
+    const {auth} = useContext(MyContext);
+    // const [user] = useAuthState(auth);
     return (
         <div>
             <h2>Authorized User!</h2>
+            <button type='button' onClick={() => auth.signOut()}>Sign Out</button>
         </div>
     )
 }
