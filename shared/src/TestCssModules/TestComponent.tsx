2df24866bbe1f5d133c662f4ad42470b0046bbcb
alegaagafonov <aga.alega@gmail.com>
2021-06-15 09:31:04 +0300

    add functionality for the shared modules

---
+++ shared/src/TestCssModules/TestComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './styles.css'
+import { TestComponentProps } from "./TestComponent.types";
+
+//import "./TestComponent.scss";
+const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
+  <div className={styles.testComponent}>
+    <h1 className={styles.testComponent}>I'm the test v2</h1>
+  </div>
+);
+
+export default TestComponent;
