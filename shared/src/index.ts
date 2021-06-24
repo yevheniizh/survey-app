1e348a465116d786ed6e0020c657d5c3fe59cc13
alegaagafonov <aga.alega@gmail.com>
2021-06-24 13:09:43 +0300

    add shared api

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,5 @@
 import TestComponent from "./TestComponent";
 import TestCssModules from './TestCssModules';
+export * from './api/index';
+export {db} from './api/config'
 export { TestComponent, TestCssModules };
