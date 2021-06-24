6a71b5bdf4fb8da3f2e9c6fc8f57186af8ca1c1e
Igor Nepomyashchyy <52207113+IgorNep@users.noreply.github.com>
2021-06-24 14:36:09 +0300

    Update index.ts

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,7 +1,6 @@
 import TestComponent from './TestComponent';
 import TestCssModules from './TestCssModules';
+import SurveyComponent from './SurveyComponent';
 export * from './api/index';
 export {db} from './api/config'
-export { TestComponent, TestCssModules };
-import SurveyComponent from './SurveyComponent';
 export { TestComponent, TestCssModules, SurveyComponent };
