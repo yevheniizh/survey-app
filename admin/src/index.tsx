69e9247c76420bf9d266a1295e1b526aa1817032
Yevhenii Zhyrov <69813005+zzzhyrov@users.noreply.github.com>
2021-07-01 09:34:47 +0300

    Revert "Admin-page layout"

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -1,12 +1,35 @@
-import * as React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase';
+import 'firebase/auth';
 import reportWebVitals from './reportWebVitals';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDXd9tTpe3joIJNx3Bqw0WJkhBRpKLqSSU',
+  authDomain: 'admin--survey.firebaseapp.com',
+  projectId: 'admin--survey',
+  storageBucket: 'admin--survey.appspot.com',
+  messagingSenderId: '226587562987',
+  appId: '1:226587562987:web:19be7a6b3fbf24e9f4ffae',
+  measurementId: 'G-C09JY3RV7E',
+});
+
+export const MyContext = createContext({} as any);
+
+const auth = firebase.auth();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyContext.Provider
+      value={{
+        auth,
+        firebase,
+      }}
+    >
+      <App />
+    </MyContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
