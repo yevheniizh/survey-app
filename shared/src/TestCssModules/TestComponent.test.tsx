0f2ac820b2ae477667a8a1f9d4d5b0a6b9da510e
Anton Lazurko <63597446+antonlazurko@users.noreply.github.com>
2021-07-07 16:46:37 +0300

    test commit for publish

--- shared/src/TestCssModules/TestComponent.test.tsx
+++ shared/src/TestCssModules/TestComponent.test.tsx
@@ -6,6 +6,7 @@ import { TestComponentProps } from './TestComponent.types';
 
 describe('Test Component', () => {
   let props: TestComponentProps;
+  console.log('111');
 
   beforeEach(() => {
     props = {
