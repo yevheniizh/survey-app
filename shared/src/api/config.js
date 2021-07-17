9fae71e68d192b6bfb57b86bd99d4ad2006314b6
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-07-17 13:46:23 +0300

    fix: Shared folder configs change

--- shared/src/api/config.js
+++ shared/src/api/config.js
@@ -1,35 +1,56 @@
-import firebase from "firebase/app";
-import "firebase/storage";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDXd9tTpe3joIJNx3Bqw0WJkhBRpKLqSSU',
-    authDomain: 'admin--survey.firebaseapp.com',
-    projectId: 'admin--survey',
-    storageBucket: 'admin--survey.appspot.com',
-    messagingSenderId: '226587562987',
-    appId: '1:226587562987:web:19be7a6b3fbf24e9f4ffae',
-    measurementId: 'G-C09JY3RV7E',
+  apiKey: 'AIzaSyAgjDRxvnG2g5IyAlc9YghZZAzB7SrauBA',
+  authDomain: 'admin--survey-app.firebaseapp.com',
+  projectId: 'admin--survey-app',
+  storageBucket: 'admin--survey-app.appspot.com',
+  messagingSenderId: '973776769956',
+  appId: '1:973776769956:web:41926346c219e250562731',
 };
 
 const app = firebase.initializeApp(firebaseConfig);
 
-export const authenticate = () => {
-    firebase
+export const googleLogin = () => {
+  firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => {
+      return firebase
         .auth()
-        .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-        .then(() => {
-            return firebase
-                .auth()
-                .signInWithPopup(new firebase.auth.GoogleAuthProvider());
-        })
-        .catch((error) => {
-            console.error(error);
-            throw error;
-        });
+        .signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    })
+    .catch((error) => {
+      console.error(error);
+      throw error;
+    });
+};
+export const formLogin = async (event, errorActions) => {
+  event.preventDefault();
+  const { email, password } = event.target.elements;
+  try {
+    await app.auth().signInWithEmailAndPassword(email.value, password.value);
+  } catch (error) {
+    console.log(error);
+    errorActions(error);
+  }
+};
+export const formSignUp = async (event, errorActions) => {
+  event.preventDefault();
+  const { email, password } = event.target.elements;
+  try {
+    await app
+      .auth()
+      .createUserWithEmailAndPassword(email.value, password.value);
+  } catch (error) {
+    console.log(error);
+    errorActions(error);
+  }
 };
 export const fireAuth = app.auth();
+export const fireSignOut = fireAuth.signOut();
 export const db = app.firestore();
 export const st = app.storage();
-
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,11 @@
 export * from './api/index';
 export * from './Survey';
-export { db } from './api/config';
+export {
+  db,
+  st,
+  fireAuth,
+  fireSignOut,
+  formSignUp,
+  formLogin,
+  googleLogin,
+} from './api/config';
