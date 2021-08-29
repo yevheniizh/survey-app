98ce6ed384143e74a389e7aada26081fbe5b7917
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-08-29 16:13:39 +0300

    feat: Edit grid for SurveyEditor

--- admin/src/components/SurveyEditor/SurveyEditor.tsx
+++ admin/src/components/SurveyEditor/SurveyEditor.tsx
@@ -8,22 +8,24 @@ import Grid from '@material-ui/core/Grid';
 
 /** <data> prop are gotten from useReadDoc */
 
-export const SurveyEditor = (data: SurveyType) => {
+const SurveyEditor = (data: SurveyType) => {
   // @ts-ignore
   let { id } = useParams();
   const [surveyData, setSurveyData] = React.useState(data);
-  const [lang, setLang] = React.useState('en');
+  const [lang, setLang] = React.useState('ua');
   const surveyProps = { ...surveyData, lang, setLang };
   const editorProps = { id, lang, setLang, surveyData, setSurveyData };
 
   return (
-    <Grid container spacing={3} justifyContent="space-between">
-      <Grid item lg={6}>
+    <Grid container spacing={1} justifyContent="space-between">
+      <Grid item lg={5} md={5} sm={12}>
         <Editor {...editorProps} />
       </Grid>
-      <Grid item lg={6}>
+      <Grid item lg={7} md={7} sm={12}>
         <Survey {...surveyProps} />
       </Grid>
     </Grid>
   );
 };
+
+export default SurveyEditor;
