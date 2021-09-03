0cf1f641eed835f1e8ab9a6a21459f9d5f38159b
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-09-03 15:56:06 +0300

    feat: Fetch filtered by current user id data

--- admin/src/components/SurveyList/index.tsx
+++ admin/src/components/SurveyList/index.tsx
@@ -1,19 +1,22 @@
+import * as React from 'react';
 import { ReadCollection, ReadCollectionType } from './readCollection';
 import { SurveyList } from './SurveyList';
 import { surveyDefaults } from '@zzzhyrov/my-perfect-package';
 import { useHistory, Redirect } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 export const Wrapper = () => {
   let history = useHistory();
+  const { currentUser } = React.useContext(AuthContext);
 
   // @ts-ignore
-  surveyDefaults.userId = 'testUser';
+  surveyDefaults.userId = currentUser.uid;
   const props: ReadCollectionType = {
     CollectionView: SurveyList,
     collection: 'surveys',
     where: {
       field: 'userId',
       operator: '==',
-      value: 'testUser',
+      value: currentUser.uid,
     },
     defaults: surveyDefaults,
     history,
